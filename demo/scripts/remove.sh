f9702c2f554f6f1de8e4d7e9dc8238647487d845
YeongYeonKim <dudtbd111@naver.com>
2021-06-16 14:43:57 +0900

    Create Repository

---
+++ demo/scripts/remove.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+find /opt/codedeploy-agent/deployment-root/배포디렉토리/* -maxdepth 0 -type 'd' | grep -v $(stat -c '%Y:%n' /opt/codedeploy-agent/deployment-root/배포디렉토리/* | sort -t: n | tail -1 | cut -d: -f2- | cut -c 3-) | xargs rm -rf
+sudo rm -rf /home/ec2-user/*
