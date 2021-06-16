34ed8f4908f99b24d91f37254f745583f868abe8
YeongYeonKim <dudtbd111@naver.com>
2021-06-16 17:18:02 +0900

    Add : cicd-test

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@GetMapping("/")
 	public String test() {
-		return "ok";
+		return "cicd-ok-2021-06-16";
 	}
 }
